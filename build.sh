25935b3d99670aefa743148fd489f9dca587ccc6
riyazm418 <119249519+riyazm418@users.noreply.github.com>
2024-05-19 15:02:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 docker build -t devops-build:v1.0 .
-docker login -u riyazm418 -p Rahila@100
-docker tag devops-build:v1.0 riyazm418/dev:devops-buil-v1.0
-docker push riyazm418/dev:devops-buil-v1.0
-
-docker login -u riyazm418 -p Rahila@100
-docker tag devops-build:v1.0 riyazm418/prod:devops-buil-v1.0
-docker push riyazm418/prod:devops-buil-v1.0
