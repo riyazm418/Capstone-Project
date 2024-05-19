536ffb2c2d5f6ada30b3d80e5d1f07a8c425dcb0
riyazm418 <119249519+riyazm418@users.noreply.github.com>
2024-05-19 11:23:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,5 @@
-# #!/bin/bash
-# echo "Building Docker image..."
-# docker build -t devops-build:v1.0 .
-
-# echo "Logging into Docker..."
-# docker login -u riyazm418 -p Rahila@100
-
-# echo "Pushing Docker image to repository..."
-# docker tag devops-build:v1.0 riyazm418/dev:devops-buil-v1.0
-# docker push riyazm418/dev:devops-buil-v1.0
-# docker tag devops-build:v1.0 riyazm418/prod:devops-buil-v1.0
-# docker push riyazm418/prod:devops-buil-v1.0
-
-
-
 #!/bin/bash
 BRANCH_NAME='git rev-parse --abbrev-ref HEAD'
-#IMAGE_TAG="riyazm418/devops-build:latest"
 echo $BRANCH_NAME
 
 if [ "$BRANCH_NAME" == "master" ]; then
@@ -31,6 +15,4 @@ docker build -t $IMAGE_TAG .
 docker login -u riyazm418 -p Rahila@100
 
 # echo "Pushing Docker image to repository..."
-# docker tag devops-build:dev riyazm418/dev:devops-build-dev
-# docker tag devops-build:prod riyazm418/prod:devops-build-prod
 docker push $IMAGE_TAG
