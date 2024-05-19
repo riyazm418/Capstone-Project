851ffc6ef8adb97ead4bcf3a7d35a890613fa798
riyazm418 <119249519+riyazm418@users.noreply.github.com>
2024-05-19 13:09:43 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,16 +3,21 @@ BRANCH_NAME='git rev-parse --abbrev-ref HEAD'
 echo $BRANCH_NAME
 
 if [ "$BRANCH_NAME" == "master" ]; then
-  IMAGE_TAG="riyazm418/prod:devops-build-v1.0"
+  docker build -t devops-build:v1.0
+  docker login -u riyazm418 -p Rahila@100
+  docker tag devops-build:v1.0 riyazm418/prod:devops-buil-v1.0
+  docker push riyazm418/prod:project01
 else
-  IMAGE_TAG="riyazm418/dev:dev-v1"
+  docker build -t devops-build:v1.0
+  docker login -u riyazm418 -p Rahila@100
+  docker tag devops-build:v1.0 riyazm418/dev:devops-buil-v1.0
+  docker push riyazm418/dev:project02
 fi
 
 # echo "Building Docker image with tag ${IMAGE_TAG}..."
-docker build -t $IMAGE_TAG .
+# docker build -t $IMAGE_TAG .
 
 # echo "Logging into Docker..."
-docker login -u riyazm418 -p Rahila@100
 
 # echo "Pushing Docker image to repository..."
-docker push $IMAGE_TAG
+# docker push $IMAGE_TAG
